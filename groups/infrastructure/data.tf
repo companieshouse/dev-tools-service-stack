1c9d4e3da7ea8b4b2dceeeb6d2e511d16c56d901
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2025-11-28 10:14:20 +0000

    CC-2573 Initial commit to create the ECS cluster

---
+++ groups/infrastructure/data.tf
@@ -0,0 +1,38 @@
+data "vault_generic_secret" "secrets" {
+  path = "applications/${var.aws_profile}/${var.environment}/${local.stack_fullname}"
+}
+
+data "aws_subnets" "application" {
+  filter {
+    name   = "tag:Name"
+    values = [local.application_subnet_pattern]
+  }
+  filter {
+    name   = "tag:NetworkType"
+    values = ["private"]
+  }
+}
+
+data "aws_subnet" "application" {
+  for_each = toset(data.aws_subnets.application.ids)
+  id       = each.value
+}
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [local.vpc_name]
+  }
+}
+
+data "aws_acm_certificate" "cert" {
+  domain = var.cert_domain
+}
+
+data "aws_ec2_managed_prefix_list" "admin" {
+  name = "administration-cidr-ranges"
+}
+
+data "aws_ec2_managed_prefix_list" "shared_services_management" {
+  name = "shared-services-management-cidrs"
+}
